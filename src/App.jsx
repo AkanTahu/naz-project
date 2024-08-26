9591681ee7e15122c52fb5db517821b0313486cb
ChamzalIsal <hhamzalnur10@gmail.com>
2024-08-26 15:49:45 +0700

    add shadcn UI
    add component translate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import Header from './materials/Header'
+import Translate from './materials/Translate'
+
 
 export default function App() {
   return (
     <>
       <Header />
+      <Translate />
+
     </>
   )
 }
--- src/materials/Translate.jsx
+++ src/materials/Translate.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Textarea } from "@/components/ui/textarea"
 
 export default function Translate() {
     return (
       <div className="container justify-center">
+        <Textarea />
+        
+        <Textarea />
         
       </div>
     )
