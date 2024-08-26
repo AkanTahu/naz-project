78fada4f2b6074245095331a444686da9069deee
ChamzalIsal <hhamzalnur10@gmail.com>
2024-08-26 13:43:11 +0700

    first commit
    first setup + first setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <div className="container justify-center">
+      <h1 className="text-3xl font-bold text-center">
+        Translate Sandi😁
+      </h1>
+    </div>
+  )
+}
