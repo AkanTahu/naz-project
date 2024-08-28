fd4588a19919332e1f269ed1b1c712bce0abddb3
ChamzalIsal <hhamzalnur10@gmail.com>
2024-08-28 13:37:59 +0700

    add: button component Translate
    - add menubar from shadcn (not-used)
    - add button from flowbite

--- src/materials/Header.jsx
+++ src/materials/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="container justify-center">
+    <div className="container justify-center mt-10">
       <h1 className="text-3xl font-bold text-center">
         Translate Sandi😁
       </h1>
--- src/materials/Translate.jsx
+++ src/materials/Translate.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Textarea } from "@/components/ui/textarea"
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
 
 export default function Translate() {
     return (
-      <div className="container justify-center">
-        <Textarea />
-        
-        <Textarea />
-        
+      <>
+      <div className="justify-center flex mt-5">
+      <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mr-3">AN</button>
+      <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mr-3">AZ</button>
+      <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mr-3">NAZ</button>
+      <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">ZAN</button>
       </div>
+      <div className="container justify-center flex mt-8">
+        <Textarea className="mr-3" /> 
+        <Textarea className="ml-3"/>   
+      </div>
+      </>
+      
     )
   }
