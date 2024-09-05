4d68428db86202dbd526a7f01b0f0d6cf51fec9a
ChamzalIsal <hhamzalnur10@gmail.com>
2024-09-06 06:49:57 +0700

    add: footer and about
    - not yet copy pop out
    - not yet background
    - not yet  change language

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./materials/Header";
 import Translate from "./materials/Translate";
 import Explanation from "./materials/Explanation";
+import Footer from "./materials/Footer";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header />
       <Translate />
       <Explanation />
+      <Footer />
     </>
   );
 }
--- src/materials/Explanation.jsx
+++ src/materials/Explanation.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 
 export default function Explanation() {
   return (
-    <div className="container justify-center mt-10">
+    <div className="container justify-center mt-10 mb-5">
       <h1 className="text-3xl font-supreme font-black text-center">
         Penjelasan📜
       </h1>
@@ -14,12 +14,14 @@ export default function Explanation() {
         <span className="font-black">AN</span> dan{" "}
         <span className="font-black">AZ</span> . Memecahkan sandi ini ialah
         sangat mudah hanya cukup memetakan semua alfabet dibagi separuh dan
-        dicocokkan antar sesama huruf. Nah sekarang mimin punya pertayaan bagaimana kalau sandi <span className="font-black">NAZ</span> ?, ada yang bisa nebak bagaimana memecahkannya wkwkwk.
+        dicocokkan antar sesama huruf. Nah sekarang mimin punya pertayaan bagaimana kalau sandi <span className="font-black">NAZ</span> ?, ada yang bisa nebak bagaimana memecahkannya wkwk.
       </h3>
       <div className="flex justify-center mt-3 mb-10">
         <img src="../public/AN.png" alt="" className="w-2/5 mr-2" />
         <img src="../public/AZ.png" alt="" className="w-2/5 ml-2" />
       </div>
+      <h1 className="text-3xl font-supreme font-black text-center">Tentang🥽</h1>
+      <h3 className="font-supreme mt-3 text-lg px-44 text-center">Aku membuat website translate sandi ini karena aku suka membuat surat bersandi dan suka membagikannya ke teman-teman aku hehe. Website ini terinspirasi dari project github <a className="underline underline-offset-8 text-teal-700" href="https://github.com/qois51">qois51</a> . shout out to him 🙌</h3>
     </div>
   );
 }
